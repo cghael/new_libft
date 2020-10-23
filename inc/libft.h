435219ac8a602cd74fb1ea9757ddee714930eb79
cghael <a.p.gorobets@gmail.com>
2020-10-23 14:13:33 +0300

    ft_check_name_comment_size() in progress....

--- inc/libft.h
+++ inc/libft.h
@@ -214,7 +214,6 @@ int						ft_strnequ(char const *s1, char const *s2, size_t n);
 char					*ft_strsub(char const *s, unsigned int start, \
 																	size_t len);
 char					*ft_strjoin(char const *s1, char const *s2);
-char					*ft_strjoin_n_free(char *s1, char *s2);
 char					*ft_strtrim(char const *s);
 char					*ft_itoa(int n);
 char					**ft_strsplit(char const *s, char c);
